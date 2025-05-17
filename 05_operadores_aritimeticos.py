5b95bddc9ac419b972ac6dbfa32503aa9f171f8c
Mauricio Celes <mauricioceles@gmail.com>
2025-05-16 23:14:27 -0300

    Acréscimo de mais aulas de python

---
+++ 05_operadores_aritimeticos.py
@@ -0,0 +1,14 @@
+produto_1 = 20
+produto_2 = 10
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 * produto_2)
+print(produto_1 % produto_2)
+print(produto_1 ** produto_2)
+
+
+y = (2 * 5) + (8 / 4) - (25 // 5)
+print(y)
